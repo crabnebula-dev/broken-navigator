ef6913f0e97cd54af7e81a6e6b9abca74ef5fa9a
Chip Reed <chip@crabnebula.dev>
2023-11-02 06:50:22 +0100

    change back password

--- challenges/space-navigator/1/src-tauri/src/commands.rs
+++ challenges/space-navigator/1/src-tauri/src/commands.rs
@@ -3,7 +3,7 @@ use tracing::*;
 #[instrument(level = "trace")]
 #[tauri::command]
 pub fn check_password(password: &str) -> bool {
-    let result = password == "Asdf";
+    let result = password == "foobar1234567";
 
     info!(
         "The Navigator checked password {password} and it was {}",
