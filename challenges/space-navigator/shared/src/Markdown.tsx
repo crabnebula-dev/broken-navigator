cdf5250194cba1646426127e9877809f571332ca
Tillmann <112912081+tillmann-crabnebula@users.noreply.github.com>
2023-12-18 19:56:28 +0900

    Fix dompurify blank target

--- challenges/space-navigator/shared/src/Markdown.tsx
+++ challenges/space-navigator/shared/src/Markdown.tsx
@@ -13,6 +13,15 @@ const render = (markdown: string) => {
   return DOMPurify.sanitize(dangerous);
 };
 
+// Allow blank target to allow link opener in browser
+DOMPurify.addHook('afterSanitizeAttributes', function (node) {
+  // set all elements owning target to target=_blank
+  if ('target' in node) {
+    node.setAttribute('target', '_blank');
+    node.setAttribute('rel', 'noopener');
+  }
+});
+
 const Markdown: VoidComponent<MarkdownProps> = ({ markdown }) => {
   return (
     <article
