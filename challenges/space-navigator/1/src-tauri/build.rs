a29044fdbe8ba5f14a4ff8bcd02172b2ff1b92e1
konstantin-crabnebula <konstantin@crabnebula.dev>
2024-01-17 12:57:27 +0100

    Dynamic challenge 1 flags

--- challenges/space-navigator/1/src-tauri/build.rs
+++ challenges/space-navigator/1/src-tauri/build.rs
@@ -1,3 +1,23 @@
+use std::fs::{read_to_string, write};
+use rand::Rng;
+use rand::distributions::Alphanumeric;
+
 fn main() {
+     // Generate Random Flag
+    let mut flag: String = String::from("Flag-");
+    let rand_str: String = rand::thread_rng()
+    .sample_iter(&Alphanumeric)
+    .take(8)
+    .map(char::from)
+    .collect();
+    flag.push_str(&rand_str);
+    println!("cargo:rustc-env=FLAG={}", flag);
+
+    //Change Flag in the log file
+    let path = concat!(env!("CARGO_MANIFEST_DIR"),"/templates/navigator.log");
+    let template = read_to_string(path).expect("Unable to read template file");
+    let result = template.replace("{{FLAG}}", &flag);
+    write("./logs/navigator.log", result).expect("Unable to write file");
+
     tauri_build::build()
 }
